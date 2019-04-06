3a05cbaa68bc1b8a037421b84f9cb2da8012b24d
GermanHC <ghcaunat@gmail.com>
2019-04-06 18:16:11 +0200

    Some changes

--- app/src/main/java/io/keepcoding/todo/ui/tasks/TaskFragment.kt
+++ app/src/main/java/io/keepcoding/todo/ui/tasks/TaskFragment.kt
@@ -8,8 +8,6 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
